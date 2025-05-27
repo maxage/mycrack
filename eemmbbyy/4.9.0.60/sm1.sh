c8f951992dda6410585c82c448b5a2e2a7f8f83f
Zhang Jian <42706238+maxage@users.noreply.github.com>
2025-05-28 01:59:37 +0800

    Create sm1.sh

---
+++ eemmbbyy/4.9.0.60/sm1.sh
@@ -0,0 +1,23 @@
+echo "-------------------web 端开心-------------------"
+echo "使用脚本前，请先进入到 emby 的 system 目录下"
+echo "官方包直接安装的路径为：cd /opt/emby-server/system/"
+echo ""
+echo "docker 安装请自己找路径进入再执行"
+echo "------------------- 开始执行 -------------------"
+rm -f Emby.Web.dll
+wget https://service.hexsen.com/emby/crackbeta/Emby.Web.dll
+rm -f MediaBrowser.Model.dll
+wget https://service.hexsen.com/emby/crackbeta/MediaBrowser.Model.dll
+rm -f MediaBrowser.Controller.dll
+wget https://service.hexsen.com/emby/crackbeta/MediaBrowser.Controller.dll
+echo "核心开心完成..."
+cd dashboard-ui/embypremiere/
+rm -f embypremiere.js
+wget https://service.hexsen.com/emby/crackbeta/dashboard-ui/embypremiere/embypremiere.js
+chmod 644 embypremiere.js
+echo "web 开心完成..."
+cd ../../
+rm -f Emby.Server.Implementations.dll
+wget https://service.hexsen.com/emby/crackbeta/Emby.Server.Implementations.dll
+echo "Implementations 替换认证..."
+chmod 644 Emby.Web.dll Emby.Server.Implementations.dll MediaBrowser.Model.dll MediaBrowser.Controller.dll
